d82aa73c07f504ff733480f2a27a84ac42861998
Vlad Kisialeuski <75371184+Kisel-kisel@users.noreply.github.com>
2023-01-07 20:49:02 +0100

    Add files via upload

--- homeWork4/Main.java
+++ homeWork4/Main.java
@@ -6,22 +6,8 @@ import homeWorks.homeWork4.Worker;
 public class Main {
     public static void main(String[] args) {
         Worker worker = new Worker();
-        Pensioner pensioner = new Pensioner();
-
 
         System.out.println(worker.calculatePension());
 
-        double example = pensioner.setPension(367.5);
-//        pensioner.setAgeOfPerson(70);
-//
-        System.out.println(pensioner.getAgeOfPerson());
-        System.out.println(example);
-
-
-
-
-
-        pensioner.die();
-        worker.die();
     }
 }
